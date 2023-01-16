e4f7d70d8fe3355de524daf22f8d211262761369
equaliser0 <accounts@equaliser.minimalcity.de>
2023-01-16 13:00:54 +0100

    fix Buffer

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from "path";
+import nodePolyfills from 'vite-plugin-node-stdlib-browser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,11 +9,24 @@ export default defineConfig({
     commonjsOptions: {
       include: ["tailwind.config.js", "node_modules/**"],
     },
+  	// rollupOptions: {
+  	// 	plugins:
+    //     [
+    //       inject({
+    //       	include: ['node_modules/@liskhq/lisk-api-client/dist-node/index.js'],
+    //       	modules: { Buffer: ['buffer', 'buffer'], }
+    //       })
+    //     ],
+  	// }
   },
   optimizeDeps: {
     include: ["tailwind-config"],
+    // exclude: ["@liskhq/lisk-api-client"]
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    nodePolyfills()
+  ],
   resolve: {
     alias: {
       "tailwind-config": path.resolve(__dirname, "./tailwind.config.js"),
