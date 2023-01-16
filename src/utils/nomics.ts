79ab3154ddb8ad1488f6e207c020950163602777
equaliser0 <accounts@equaliser.minimalcity.de>
2023-01-16 13:44:32 +0100

    prepare nomics connector

---
+++ src/utils/nomics.ts
@@ -0,0 +1,16 @@
+const { apiClient, cryptography } = require('@liskhq/lisk-client');
+const RPC_ENDPOINT = 'ws://localhost:8080/ws';
+
+let clientCache;
+
+export const getClient = async () => {
+    if (!clientCache) {
+        clientCache = await apiClient.createWSClient(RPC_ENDPOINT);
+    }
+    return clientCache;
+};
+
+export const fetchAccountInfo = async (address) => {
+    const client = await getClient();
+    return client.account.get(cryptography.getAddressFromBase32Address(address));
+};
