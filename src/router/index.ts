9a9bd5585eeb8f520eddff59ccd3e6fa179798f3
equaliser0 <accounts@equaliser.minimalcity.de>
2023-06-13 22:43:45 +0200

    add Scrollbehaviour

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/create',
+    name: 'create',
+    component: () => import('../views/CreateBusiness.vue')
+  },
+  {
+    path: '/wallet',
+    name: 'wallet',
+    component: () => import('../views/Wallet.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/Login.vue')
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/create',
-      name: 'create',
-      component: () => import('../views/CreateBusiness.vue')
-    },
-    {
-      path: '/wallet',
-      name: 'wallet',
-      component: () => import('../views/Wallet.vue')
-    },
-    {
-      path: '/login',
-      name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Login.vue')
-    }
-  ]
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    return savedPosition || { left: 0, top: 0 };
+  },
 })
 
 export default router
