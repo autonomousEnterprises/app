2153919c30fa20615ac2b521496c94fbc43c00e8
equaliser0 <accounts@equaliser.minimalcity.de>
2023-06-13 22:43:28 +0200

    edit data store

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
-import BusinessList from '../components/BusinessList.vue'
+import Marketplace from '../components/Marketplace.vue'
 import { useNotificationStore } from '../stores/notifications';
+import { useMarketplaceStore } from '../stores/marketplace';
 
+const marketplaceStore = useMarketplaceStore()
 const notificationStore = useNotificationStore()
 
 const createBusiness = ref(false)
@@ -30,19 +32,19 @@ const listing = ref(false)
 <template>
   <main class="flex flex-wrap justify-center overflow-scroll">
     <div class="text-center p-4 w-full">
-      <h1>Network Performance</h1>
+      <h1 class="text-2xl font-bold text-center pb-8">Network Performance</h1>
     </div>
     <div class="stats stats-vertical lg:stats-horizontal hadow m-4 card w-96 bg-base-100 p-4 m-2">
       <div class="stat place-items-center">
         <div class="stat-title">Users</div>
         <div class="stat-value text-success">70</div>
-        <div class="stat-desc text-success">↗︎ 40 (2%)</div>
+        <div class="stat-desc text-success tooltip" data-tip="Since 1 week">↗︎ 40 (2%)</div>
       </div>
 
       <div class="stat place-items-center">
         <div class="stat-title">Waitlist</div>
         <div class="stat-value text-success">123</div>
-        <div class="stat-desc text-success">↗︎ 122 (14%)</div>
+        <div class="stat-desc text-success tooltip" data-tip="Since 1 week">↗︎ 122 (14%)</div>
         <!-- <div class="stat-desc">↘︎ 90 (14%)</div> -->
       </div>
     </div>
@@ -50,7 +52,7 @@ const listing = ref(false)
     <div class="stats shadow m-4 card w-96 bg-base-100 p-4 m-2">
       <div class="stat place-items-center">
         <div class="stat-title">Created Organisations</div>
-        <div class="stat-value">5+</div>
+        <div class="stat-value">{{ marketplaceStore.businesses.length }}</div>
         <div class="stat-desc">Testnet</div>
       </div>
     </div>
@@ -60,15 +62,17 @@ const listing = ref(false)
     </div>
 
 
-    <div class="hero-content text-center">
+    <!-- <div class="hero-content text-center">
       <div class="max-w-md">
         <h1 class="text-5xl font-bold">Hello there</h1>
         <p class="py-6">You have no created businesses or joined any. </p>
         <button class="btn btn-primary" @click="">Get Started</button>
       </div>
-    </div>
-    <!-- <div class="flex justify-center p-4 w-full">
-      <BusinessList/>
     </div> -->
+
+    <div class="flex flex-col justify-center p-4 w-full card bg-base-100">
+      <h2 class="text-2xl font-bold text-center pb-8">Marketplace</h2>
+      <Marketplace/>
+    </div>
   </main>
 </template>
