380e80312ca879cddbef2f22fc068a14dfd715c4
equaliser0 <accounts@equaliser.minimalcity.de>
2023-06-15 16:59:18 +0200

    facelift

--- src/App.vue
+++ src/App.vue
@@ -17,27 +17,27 @@ const notificationStore = useNotificationStore()
 
 const getNotifcationType = (notificationType) => {
   if (notificationType === 'standard') {
-    return 'bg-base-100'
+    return 'bg-base-100';
   };
   if (notificationType === 'success') {
-    return 'alert-success'
+    return 'alert-success';
   };
   if (notificationType === 'warning') {
-    return 'alert-warning'
+    return 'alert-warning';
   };
   if (notificationType === 'error') {
-    return 'alert-error'
+    return 'alert-error';
   };
 }
 </script>
 
 <template>
-  <div class="min-h-screen w-full bg-base-200" :data-theme="themeStore.theme">
+  <div class="min-h-screen w-full bg-base-300" :data-theme="themeStore.theme">
     <div class="drawer">
       <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
       <div class="drawer-content flex flex-col">
         <!-- Navbar -->
-        <div class="w-full navbar bg-base-300 shadow fixed z-40">
+        <div class="w-full navbar backdrop-blur-sm bg-base-100/50 shadow fixed z-40">
           <div class="flex-none lg:hidden">
             <label for="my-drawer-3" class="btn btn-square btn-ghost">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
@@ -45,9 +45,9 @@ const getNotifcationType = (notificationType) => {
           </div>
           <div class="flex-1 px-2 mx-2">ecosis</div>
           <div class="flex-none hidden lg:block">
-            <ul class="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+            <ul class="menu menu-vertical lg:menu-horizontal bg-base-100 rounded-box">
               <li><RouterLink to="/"><LayoutDashboard/>Hub</RouterLink></li>
-              <li><RouterLink to="/create"><PlusSquare/>Create</RouterLink></li>
+              <li><RouterLink to="/deployer"><PlusSquare/>Deploy</RouterLink></li>
               <li><RouterLink to="/wallet"><Wallet/>Wallet</RouterLink></li>
             </ul>
             <ul class="menu menu-horizontal">
@@ -68,7 +68,7 @@ const getNotifcationType = (notificationType) => {
         <ul class="menu p-4 w-80 h-full bg-base-200">
           <!-- Sidebar content here -->
             <li><RouterLink to="/"><LayoutDashboard/>Hub</RouterLink></li>
-            <li><RouterLink to="/create"><PlusSquare/>Create</RouterLink></li>
+            <li><RouterLink to="/deployer"><PlusSquare/>Deploy</RouterLink></li>
             <li><RouterLink to="/wallet"><Wallet/>Wallet</RouterLink></li>
             <div class="divider"></div>
             <ThemeSelect/>
