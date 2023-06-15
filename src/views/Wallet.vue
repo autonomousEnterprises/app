082acd1cc44e940b480ef03cca9800d3e3c42fe5
equaliser0 <accounts@equaliser.minimalcity.de>
2023-06-15 16:59:01 +0200

    prepare wallet functionalities

--- src/views/Wallet.vue
+++ src/views/Wallet.vue
@@ -1,70 +1,59 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
-import BusinessList from '../components/BusinessList.vue'
+import { useWalletStore } from '../stores/wallet';
 
-const createBusiness = ref(false)
-const tokenise = ref(false)
-const enableVM = ref(false)
-const assistance = ref(false)
-const autoBranding = ref(false)
-const autoMarketNiche = ref(false)
-const autoBusinessModel = ref(false)
-const autoProductDesign = ref(false)
-const autopilot = ref(false)
+const walletStore = useWalletStore()
 
-const branding = ref({
-  name: '',
-  tagline: '',
-  url: '',
-  category: 'Community Pool',
-  model: '',
-})
+const formatCurrency = (number: number) => {
+  if (number) {
+    const formattedNumber = number.toString().replace(/\D/g, "");
+    const rest = formattedNumber.length % 3;
+    let currency = formattedNumber.substr(0, rest);
+    const thousand = formattedNumber.substr(rest).match(/\d{3}/g);
+    let separator;
 
-const listing = ref(false)
+    if (thousand) {
+      separator = rest ? "," : "";
+      currency += separator + thousand.join(",");
+    }
+
+    return currency;
+  } else {
+    return "";
+  }
+};
 </script>
 
 <template>
-  <main class="flex flex-wrap justify-center overflow-scroll">
-    <section class="flex flex-col justify-center">
-      <div class="stats stats-vertical lg:stats-horizontal bg-base-100 text-primary-content">
+  <main class="flex">
+    <section class="flex flex-col justify-start">
+      <div class="stats stats-vertical lg:stats-horizontal bg-base-100 card text-primary-content shadow h-1/3">
 
-        <div class="stat">
-          <div class="stat-title">Account balance</div>
+        <div class="stat flex flex-col justify-center">
+          <div class="stat-title">Total balance</div>
           <div class="stat-value">$89,400</div>
+
+          <div class="stat-title">Total assets: <span class="bold">{{ walletStore.wallets.length }}</span></div>
           <div class="stat-actions">
             <button class="btn btn-sm btn-success">Add funds</button>
           </div>
         </div>
 
-        <div class="stat">
-          <div class="stat-title">Current balance</div>
-          <div class="stat-value">$89,400</div>
-          <div class="stat-actions">
-            <button class="btn btn-sm">Withdrawal</button>
-            <button class="btn btn-sm">deposit</button>
+        <div class="stats stats-vertical overflow-scroll">
+
+          <div class="stat" v-for="wallet in walletStore.wallets">
+            <div class="stat-title">Current balance</div>
+            <div class="stat-value">{{ formatCurrency(wallet.balance) }}</div>
+            <div class="stat-actions">
+              <button class="btn btn-sm">Withdrawal</button>
+              <button class="btn btn-sm">deposit</button>
+              <div class="stat-actions">
+                <button class="btn btn-sm btn-primary w-full">Send</button>
+              </div>
+            </div>
           </div>
-        </div>
 
-      </div>
-      <div class="stats shadow mt-8">
-
-        <div class="stat place-items-center">
-          <div class="stat-title">Downloads</div>
-          <div class="stat-value">31K</div>
-          <div class="stat-desc">From January 1st to February 1st</div>
-        </div>
-
-        <div class="stat place-items-center">
-          <div class="stat-title">Users</div>
-          <div class="stat-value text-success">4,200</div>
-          <div class="stat-desc text-success">↗︎ 40 (2%)</div>
-        </div>
-
-        <div class="stat place-items-center">
-          <div class="stat-title">New Registers</div>
-          <div class="stat-value">1,200</div>
-          <div class="stat-desc">↘︎ 90 (14%)</div>
         </div>
 
       </div>
