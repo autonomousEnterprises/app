8d86c0139699c5a075260973ac66a834da18b200
equaliser0 <accounts@equaliser.minimalcity.de>
2023-06-15 16:57:36 +0200

    rename & extend

---
+++ src/stores/deployer.ts
@@ -0,0 +1,74 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios';
+import { useNotificationStore } from './notifications';
+import { useWalletStore } from './wallet';
+
+const notificationStore = useNotificationStore()
+const walletStore = useWalletStore()
+
+const instance = axios.create({
+  baseURL: 'http://localhost:3000',
+  timeout: 20000,
+  // headers: {'X-Custom-Header': 'foobar'}
+});
+
+export const useDeployerStore = defineStore('deployer', () => {
+  const branding = ref({
+
+  })
+
+  const token = ref({
+    // name: '',
+    // symbol: '',
+    // supply: '',
+    // fee: '',
+  })
+
+  function deployBusiness() {
+    try {
+
+    } catch (error) {
+      throw new Error(error)
+    }
+    // brandinges.value.push(branding)
+  }
+
+  async function deployToken() {
+    try {
+      if (!token.value.name && !token.value.symbol && !token.value.supply && !token.value.fee) {
+        throw new Error('Please fill all token parameter!')
+      }
+
+      const res = await instance.post('/deploy', token)
+
+      notificationStore.addNotification({
+        type: 'success',
+        msg: 'Successfully deployed!'
+      })
+
+      walletStore.addWallet(res.data.genesisWallet)
+    } catch (error) {
+      console.log(error);
+
+      if (error.response && error.response.status === 403) {
+        return notificationStore.addNotification({
+          type: 'error',
+          msg: 'Token already exists'
+        })
+      }
+
+      notificationStore.addNotification({
+        type: 'error',
+        msg: error.message
+      })
+    }
+  }
+
+  function deploy() {
+    deployBusiness()
+    deployToken()
+  }
+
+  return { branding, token, deployBusiness, deployToken, deploy }
+})
