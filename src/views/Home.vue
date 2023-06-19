80257237f277973918721a4f3ad77c3dacad1e0e
equaliser0 <accounts@equaliser.minimalcity.de>
2023-06-19 20:42:07 +0200

    add new metrics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,8 @@ import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
 import {
   CandlestickChart,
-  Building2
+  Building2,
+  User
 } from 'lucide-vue-next';
 import Marketplace from '../components/Marketplace.vue'
 import { useNotificationStore } from '../stores/notifications';
@@ -22,44 +23,97 @@ const autoBusinessModel = ref(false)
 const autoProductDesign = ref(false)
 const autopilot = ref(false)
 
-const branding = ref({
-  name: '',
-  tagline: '',
-  url: '',
-  category: 'Community Pool',
-  model: '',
-})
+const branding = ref({})
 
 const listing = ref(false)
 </script>
 
 <template>
-  <main class="flex flex-wrap justify-center overflow-scroll">
+  <main class="flex flex-col justify-center overflow-scroll">
     <div class="text-center p-4 w-full flex justify-center">
       <CandlestickChart class="mr-2 mt-5"/>
       <h1>Network Performance</h1>
     </div>
-    <div class="stats shadow stats-vertical lg:stats-horizontal hadow m-4 card w-96 bg-base-100 p-4 m-2">
-      <div class="stat place-items-center">
-        <div class="stat-title">Users</div>
-        <div class="stat-value text-success">70</div>
-        <div class="stat-desc text-success tooltip" data-tip="Since 1 week">↗︎ 40 (2%)</div>
+    <div class="w-full flex justify-center">
+      <select class="select select-bordered select-md w-full max-w-xs">
+        <option selected>Nomics</option>
+        <option disabled>More soon</option>
+      </select>
+    </div>
+    <div class="flex flex-wrap justify-center">
+      <div class="stats shadow stats-vertical hadow m-4 card w-96 bg-base-100 p-4 m-2 flex">
+        <div class="stat place-items-center">
+          <div class="stat-title">Users</div>
+          <div class="stat-value text-success">70</div>
+          <div class="stat-desc text-success tooltip" data-tip="Since 1 week">↗︎ 40 (2%)</div>
+        </div>
+
+        <div class="stat place-items-center">
+          <div class="stat-title">Waitlist</div>
+          <div class="stat-value text-success">123</div>
+          <div class="stat-desc text-success tooltip" data-tip="Since 1 week">↗︎ 122 (14%)</div>
+          <!-- <div class="stat-desc">↘︎ 90 (14%)</div> -->
+        </div>
+
+        <div class="stat place-items-center">
+          <div class="stat-title">Organisations</div>
+          <div class="stat-value text-success">{{ marketplaceStore.businesses.length }}</div>
+          <div class="stat-desc text-success tooltip" data-tip="Since 1 week">↗︎ 122 (14%)</div>
+          <div class="stat-desc">Testnet</div>
+        </div>
       </div>
 
-      <div class="stat place-items-center">
-        <div class="stat-title">Waitlist</div>
-        <div class="stat-value text-success">123</div>
-        <div class="stat-desc text-success tooltip" data-tip="Since 1 week">↗︎ 122 (14%)</div>
-        <!-- <div class="stat-desc">↘︎ 90 (14%)</div> -->
+      <div class="stats shadow stats-vertical shadow m-4 card w-96 bg-base-100 p-4 m-2 flex2">
+        <div class="stat place-items-center">
+          <div class="stat-title">Total Transaction Volume</div>
+          <div class="stat-value text-success">70</div>
+          <div class="stat-desc text-success tooltip" data-tip="Since 1 week">↗︎ 40 (2%)</div>
+        </div>
+
+        <div class="stat place-items-center">
+          <div class="stat-title">Total Deposit Volume</div>
+          <div class="stat-value text-success">123</div>
+          <div class="stat-desc text-success tooltip" data-tip="Since 1 week">↗︎ 122 (14%)</div>
+          <!-- <div class="stat-desc">↘︎ 90 (14%)</div> -->
+        </div>
       </div>
     </div>
 
-    <div class="stats shadow m-4 card w-96 bg-base-100 p-4 m-2">
-      <div class="stat place-items-center">
-        <div class="stat-title">Created Organisations</div>
-        <div class="stat-value">{{ marketplaceStore.businesses.length }}</div>
-        <div class="stat-desc">Testnet</div>
+    <div class="w-full flex flex-col justify-center items-center">
+      <div class="text-center p-4 w-full flex justify-center">
+        <User class="mr-2 mt-5"/>
+        <h1>Your Performance</h1>
       </div>
+      <div class="stats shadow stats-vertical lg:stats-horizontal shadow m-4 card w-96 bg-base-100 p-4 m-2 flex2 w-full">
+        <div class="stat place-items-center">
+          <div class="stat-title">Rank</div>
+          <div class="stat-value text-success">70</div>
+          <div class="stat-desc text-success tooltip" data-tip="Since 1 week">↗︎ 40 (2%)</div>
+        </div>
+
+        <div class="stat place-items-center">
+          <div class="stat-title">Reputation</div>
+          <div class="stat-value text-success">123</div>
+          <div class="stat-desc text-success tooltip" data-tip="Since 1 week">↗︎ 122 (14%)</div>
+          <!-- <div class="stat-desc">↘︎ 90 (14%)</div> -->
+        </div>
+        <div class="stat place-items-center">
+          <div class="stat-title">Follower</div>
+          <div class="stat-value text-success">70</div>
+          <div class="stat-desc text-success tooltip" data-tip="Since 1 week">↗︎ 40 (2%)</div>
+        </div>
+        <div class="stat place-items-center">
+          <div class="stat-title">Total Transaction Volume</div>
+          <div class="stat-value text-success">70</div>
+          <div class="stat-desc text-success tooltip" data-tip="Since 1 week">↗︎ 40 (2%)</div>
+        </div>
+        <div class="stat place-items-center">
+          <div class="stat-title">Total Deposit Volume</div>
+          <div class="stat-value text-success">70</div>
+          <div class="stat-desc text-success tooltip" data-tip="Since 1 week">↗︎ 40 (2%)</div>
+        </div>
+      </div>
+      <RouterLink class="btn btn-wide btn-standard" to="deployer">Invite & earn rewards</RouterLink>
     </div>
 
     <div class="flex justify-center p-4 w-full">
@@ -76,6 +130,14 @@ const listing = ref(false)
     </div> -->
 
     <div class="flex flex-col justify-center w-full card bg-base-100 shadow">
+      <div class="text-center p-4 w-full flex justify-center">
+        <Building2 class="mr-2 mt-4"/>
+        <h2>Your Businesses</h2>
+      </div>
+      <Marketplace/>
+    </div>
+
+    <div class="flex flex-col justify-center w-full card bg-base-100 shadow mt-8">
       <div class="text-center p-4 w-full flex justify-center">
         <Building2 class="mr-2 mt-4"/>
         <h2>Marketplace</h2>
